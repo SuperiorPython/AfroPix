f097dadd5696e737fb582df04365efb7d7add764
Bryson Wingate <bawingate@techexchange.in>
2025-02-22 13:32:18 -0500

    Add files via upload

--- AfroPix/app.py
+++ AfroPix/app.py
@@ -1,7 +1,11 @@
 import streamlit as st
 import time
 
-def stream_data():
+col1, col2, col3 = st.columns([1, 2, 1])
+
+
+
+def title_stream_data():
     intro_message = """
     Hello viewer. I see you have decided to partake in soaking in the knowledge of 
     African-American history and our achievements. Allow us to introduce you to our website.
@@ -10,19 +14,31 @@ def stream_data():
         yield word + " "
         time.sleep(0.1)
 
+def prefer_stream_data():
+    prefer_message = """
+        We are glad you have decided to learn about our achievements, you have 
+        the option of choosing between multiple topics concerning African American Achievements
+        """
+    for word in prefer_message.split(" "):
+        yield word + " "
+        time.sleep(0.1)
+
 def title_page():
-    if st.button("Hello"):
-        st.write_stream(stream_data)
-        time.sleep(2)
-        st.title(":orange[Scarab Legacy]")
+    with col2:
+        if st.button("Hello"):
+            st.write_stream(title_stream_data)
+            time.sleep(2)
+            st.title(":orange[Scarab Legacy]")
         time.sleep(2)
         if st.button("Let's Begin"):
             st.session_state.page = "preferences" # Update session state
 
 def preference_page():
-    st.write("Hello")
+    with col2:
+        st.write(prefer_stream_data)
     if st.button("Go Back"):
         st.session_state.page = "title"
+        title_page()
 
 # Initialize session state
 if 'page' not in st.session_state:
